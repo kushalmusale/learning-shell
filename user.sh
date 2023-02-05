48be164dd5e78ca8ff11356c1d5f3a8ef5c607c2
kushal <kushal.musale21@gmail.com>
2023-02-05 12:56:13 +0530

    modified catalogue.sh and user.sh files

--- user.sh
+++ user.sh
@@ -5,8 +5,14 @@ useradd roboshop
 
 curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
 cd /home/roboshop
-unzip -o /tmp/user.zip
 rm -rf user
+unzip -o /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
-npm install
+npm install 
+
+sed -i -e 's/REDIS_ENDPOINT/redis.devops69.online/' -e 's/MONGO_ENDPOINT/mongodb.devops69.online/' systemd.service 
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl restart user
+systemctl enable user
