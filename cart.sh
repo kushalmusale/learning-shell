57844f80ebda233f4dcb40f25970b17883bbd862
kushal <kushal.musale21@gmail.com>
2023-02-05 13:06:35 +0530

    updated cart.sh script

--- cart.sh
+++ cart.sh
@@ -9,4 +9,11 @@ rm -rf cart
 unzip -o /tmp/cart.zip
 mv cart-main cart
 cd cart
-npm install
+npm install 
+
+sed -i -e 's/REDIS_ENDPOINT/redis.devops69.online/' -e 's/CATALOGUE_ENDPOINT/catalog.devops69.online/' systemd.service 
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
