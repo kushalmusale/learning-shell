8ac17307136a92ea1a588d14f972807bdf3d6f14
kushal <kushal.musale21@gmail.com>
2023-02-05 08:41:17 +0530

    added mongodb script

---
+++ mongodb.sh
@@ -0,0 +1,4 @@
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo 
+yum install -y mongodb-org
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongd.cong
